6bc164be9eb65f4f6a9dd4c5d21a30be3987b78d
JayvonK <jaykhuth@gmail.com>
2024-01-26 12:39:32 -0800

    8ball html

--- scripts/reverse2.js
+++ scripts/reverse2.js
@@ -1,4 +1,4 @@
-import { Reverse1Api } from "./app.js";
+import { Reverse2Api } from "./app.js";
 
 let rv2Input = document.getElementById("rv2Input");
 let rv2Div = document.getElementById("rv2Div");
@@ -10,7 +10,7 @@ let rv2Result = document.getElementById("rv2Result");
 rv2Run.addEventListener('click', async (event) => {
     if(rv2Input.value !== "")
     {
-        let result = await Reverse1Api(rv2Input.value);
+        let result = await Reverse2Api(rv2Input.value);
         rv2Div.className = "lg:h-[275px] lg:w-[1066px] w-96 h-72 bg-[#161A30] flex items-center justify-center hidden";
         rv2Appear.className = "lg:h-[275px] lg:w-[1066px] w-96 h-72 bg-[#161A30] flex items-center justify-center";
         rv2Again.className = "lg:h-[120px] lg:w-[471px] bg-[#161A30] rounded-[100px] flex justify-center items-center hover:cursor-pointer w-56 h-16";
